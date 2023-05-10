064b575b55a7f435d29716ccd26285b5595710dc
Thuc Tran <thuctran@kms-healthcare.com>
2023-05-10 15:38:36 +0700

    add workflows build

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,12 +1,16 @@
 const scanner = require("sonarqube-scanner");
 scanner(
-  {
-    serverUrl: "http://localhost:9000",
-    login: "admin",
-    password: "admin",
-    options: {
-      "sonar.sources": "./src",
+    {
+        serverUrl: "http://localhost:9000",
+        token: "squ_6930227a62668b4ea0c346c05df8f67c9ade8a4d",
+        options: {
+            "sonar.sources": "./src",
+            "sonar.exclusions": "**/*.test.tsx",
+            "sonar.tests": "./src",
+            "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+            "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+            "sonar.testExecutionReportPaths": "test-report.xml"
+        },
     },
-  },
-  () => process.exit()
+    () => process.exit()
 );
